c8b0ed9e951bdebc38d32a140e7582235687cb47
Bill Cawthra <bill.cawthra@pokitdok.com>
2017-12-03 14:19:45 -0500

    fixed curl command

--- prometheus-compose/init.sh
+++ prometheus-compose/init.sh
@@ -4,7 +4,7 @@ echo "Waiting a few seconds to let Grafana start..."
 sleep 10
 # create prometheus datasource
 echo "Creating the datasource..."
-curl -u admin:admin 'http://grafana:3000/api/datasources' -X PUT --data-binary '{"name":"prometheus","type":"prometheus","typeLogoUrl":"public/app/plugins/datasource/prometheus/img/prometheus_logo.svg","access":"proxy","url":"http://prometheus:9090","password":"","user":"","database":"","basicAuth":false,"isDefault":true,"jsonData":{},"message":"created via curl."}'
+curl -u admin:admin 'http://grafana:3000/api/datasources' -XPOST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"prometheus","type":"prometheus","typeLogoUrl":"public/app/plugins/datasource/prometheus/img/prometheus_logo.svg","access":"proxy","url":"http://prometheus:9090","password":"","user":"","database":"","basicAuth":false,"isDefault":true,"jsonData":{},"message":"created via curl."}'
 
 # sleep infinity
 sleep infinity
