47e0874d5ad455b05c25bc83abd05459bd0a8d46
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-05-29 15:51:47 -0300

    feat: upgrade to apiv2

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -17,26 +17,39 @@ log.addHandler(ch)
 
 class CoinCollector():
   def __init__(self):
-    self.endpoint = 'https://api.coinmarketcap.com/v1/ticker/'
+    self.endpoint = 'https://api.coinmarketcap.com/v2/ticker/'
 
   def collect(self):
-    # query the api
-    r = requests.get(self.endpoint)
-    request_time = r.elapsed.total_seconds()
-    log.info('elapsed time -' + str(request_time))
-    response = json.loads(r.content.decode('UTF-8'))
-    # setup the metric
-    metric = Metric('coinmarketcap_response_time', 'Total time for the coinmarketcap API to respond.', 'summary')
-    # add the response time as a metric
-    metric.add_sample('coinmarketcap_response_time', value=float(request_time), labels={'name': 'coinmarketcap.com'})
-    yield metric
-    metric = Metric('coin_market', 'coinmarketcap metric values', 'gauge')
-    for each in response:
-      for that in ['rank', 'price_usd', 'price_btc', '24h_volume_usd', 'market_cap_usd', 'available_supply', 'total_supply', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']:
-        coinmarketmetric = '_'.join(['coin_market', that])
-        if each[that] is not None:
-          metric.add_sample(coinmarketmetric, value=float(each[that]), labels={'id': each['id'], 'name': each['name'], 'symbol': each['symbol']})
-    yield metric
+    start = 1
+    while True:
+        # query the api
+        print('starting with %d' % (start))
+        r = requests.get('%s?convert=BTC&start=%d' % (self.endpoint, start))
+        request_time = r.elapsed.total_seconds()
+        log.info('elapsed time -' + str(request_time))
+        response = json.loads(r.content.decode('UTF-8'))
+        if not response['data']:
+            break
+        # setup the metric
+        metric = Metric('coinmarketcap_response_time', 'Total time for the coinmarketcap API to respond.', 'summary')
+        # add the response time as a metric
+        metric.add_sample('coinmarketcap_response_time', value=float(request_time), labels={'name': 'coinmarketcap.com'})
+        yield metric
+        metric = Metric('coin_market', 'coinmarketcap metric values', 'gauge')
+        for _, value in response['data'].items():
+            for that in ['rank', 'total_supply', 'max_supply', 'circulating_supply']:
+                coinmarketmetric = '_'.join(['coin_market', that])
+                if value[that] is not None:
+                    metric.add_sample(coinmarketmetric, value=float(value[that]), labels={'id': value['id'], 'name': value['name'], 'symbol': value['symbol']})
+            for price in ['USD', 'BTC']:
+                for that in ['price', 'volume_24h', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']:
+                    coinmarketmetric = '_'.join(['coin_market', that, price]).lower()
+                    if value['quotes'][price] is None:
+                        continue
+                    if value['quotes'][price][that] is not None:
+                        metric.add_sample(coinmarketmetric, value=float(value['quotes'][price][that]), labels={'id': value['id'], 'name': value['name'], 'symbol': value['symbol']})
+        yield metric
+        start += len(response['data'])
 
 if __name__ == '__main__':
   try:
@@ -46,7 +59,7 @@ if __name__ == '__main__':
     parser.add_argument('--port', nargs='?', const=9101, help='The TCP port to listen on.  Defaults to 9101.', default=9101)
     args = parser.parse_args()
     log.info(args.port)
-  
+
     REGISTRY.register(CoinCollector())
     start_http_server(int(args.port))
     while True:
