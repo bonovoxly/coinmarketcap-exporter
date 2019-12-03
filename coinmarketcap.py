ddc6eee09322e0c3ddd576470a8040a3c6625a33
Bill Cawthra <bcawthra@gmail.com>
2019-12-03 10:17:25 -0500

    Lots of fixes and updates

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -13,9 +13,6 @@ import time
 # lock of the collect method
 lock = Lock()
 
-# caching API for 2min
-cache = TTLCache(maxsize=1000, ttl=120)
-
 # logging setup
 log = logging.getLogger('coinmarketcap-exporter')
 log.setLevel(logging.INFO)
@@ -26,20 +23,27 @@ ch.setFormatter(formatter)
 log.addHandler(ch)
 
 cak = os.environ.get('COINMARKETCAP_API_KEY')
+# caching API for 10min
+# Note the api limits: https://pro.coinmarketcap.com/features
+cache_ttl = os.environ.get('CACHE_TTL', 3000)
+cache = TTLCache(maxsize=10000, ttl=cache_ttl)
 
 class CoinClient():
   def __init__(self):
+
     self.url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
     self.headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': cak}
     self.parameters = {'start': '1', 'limit': '5000', 'convert': 'USD'}
 
   @cached(cache)
-  def tickers(self, start):
+  def tickers(self):
     session = Session()
     session.headers.update(self.headers)
     r = session.get(self.url, params=self.parameters)
     data = json.loads(r.text)
-    print(data)
+    if 'data' not in data:
+      log.error('No data in response. Is your API key set?')
+      log.info(data)
     return data
 
 class CoinCollector():
@@ -49,27 +53,25 @@ class CoinCollector():
   def collect(self):
     with lock:
       log.info('collecting...')
-      start = 1
-      while True:
-        # query the api
-        response = self.client.tickers(start)
-        if not response['data']:
-          break
-        metric = Metric('coin_market', 'coinmarketcap metric values', 'gauge')
-        for _, value in response['data'].items():
-          for that in ['rank', 'total_supply', 'max_supply', 'circulating_supply']:
+      # query the api
+      response = self.client.tickers()
+      metric = Metric('coin_market', 'coinmarketcap metric values', 'gauge')
+      if 'data' not in response:
+        log.error('No data in response. Is your API key set?')
+      else:
+        for value in response['data']:
+          for that in ['cmc_rank', 'total_supply', 'max_supply', 'circulating_supply']:
             coinmarketmetric = '_'.join(['coin_market', that])
             if value[that] is not None:
-              metric.add_sample(coinmarketmetric, value=float(value[that]), labels={'id': value['website_slug'], 'name': value['name'], 'symbol': value['symbol']})
-          for price in ['USD', 'BTC']:
+              metric.add_sample(coinmarketmetric, value=float(value[that]), labels={'id': value['slug'], 'name': value['name'], 'symbol': value['symbol']})
+          for price in ['USD']:
             for that in ['price', 'volume_24h', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']:
               coinmarketmetric = '_'.join(['coin_market', that, price]).lower()
-              if value['quotes'][price] is None:
+              if value['quote'][price] is None:
                 continue
-              if value['quotes'][price][that] is not None:
-                metric.add_sample(coinmarketmetric, value=float(value['quotes'][price][that]), labels={'id': value['website_slug'], 'name': value['name'], 'symbol': value['symbol']})
-        yield metric
-        start += len(response['data'])
+              if value['quote'][price][that] is not None:
+                metric.add_sample(coinmarketmetric, value=float(value['quote'][price][that]), labels={'id': value['slug'], 'name': value['name'], 'symbol': value['symbol']})
+      yield metric
 
 if __name__ == '__main__':
   try:
