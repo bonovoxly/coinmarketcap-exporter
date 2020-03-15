bf0ff9de63c9046c9c41219268cebf065761f7b1
Wojtek Siudzinski <admin@suda.pl>
2020-03-15 14:00:15 +0100

    Expose currency and max cache size as env variables

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -22,21 +22,24 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 log.addHandler(ch)
 
+currency = os.environ.get('CURRENCY', 'USD')
 cak = os.environ.get('COINMARKETCAP_API_KEY')
 # caching API for 10min
 # Note the api limits: https://pro.coinmarketcap.com/features
-cache_ttl = os.environ.get('CACHE_TTL', 3000)
-cache = TTLCache(maxsize=10000, ttl=cache_ttl)
+cache_ttl = int(os.environ.get('CACHE_TTL', 3000))
+cache_max_size = int(os.environ.get('CACHE_MAX_SIZE', 10000))
+cache = TTLCache(maxsize=cache_max_size, ttl=cache_ttl)
 
 class CoinClient():
   def __init__(self):
 
     self.url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
     self.headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': cak}
-    self.parameters = {'start': '1', 'limit': '5000', 'convert': 'USD'}
+    self.parameters = {'start': '1', 'limit': '5000', 'convert': currency}
 
   @cached(cache)
   def tickers(self):
+    log.info('Fetching data from the API')
     session = Session()
     session.headers.update(self.headers)
     r = session.get(self.url, params=self.parameters)
@@ -64,7 +67,7 @@ class CoinCollector():
             coinmarketmetric = '_'.join(['coin_market', that])
             if value[that] is not None:
               metric.add_sample(coinmarketmetric, value=float(value[that]), labels={'id': value['slug'], 'name': value['name'], 'symbol': value['symbol']})
-          for price in ['USD']:
+          for price in [currency]:
             for that in ['price', 'volume_24h', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']:
               coinmarketmetric = '_'.join(['coin_market', that, price]).lower()
               if value['quote'][price] is None:
